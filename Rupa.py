19c3de3427dee8d627d0debe71d6813208a4c055
N-Durga-Rupini-Devi <rupanamburi7@gmail.com>
2025-09-25 10:54:56 +0530

    Update Rupa.py

--- Rupa.py
+++ Rupa.py
@@ -11,4 +11,23 @@ elif op == '*':
 elif op == '/':
     print(n / m)
 else:
+
     print('Invalid operator')
+    <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+echo $txt;
+echo "<br>";
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+</body>
+</html>
